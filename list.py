0b7c0493e14ac275be08ad5551d3c51bef28b6ee
Lina-Liuna <liuna.lina@gmail.com>
2021-07-08 07:19:26 -0700

    update

--- list.py
+++ list.py
@@ -32,6 +32,9 @@ matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 rearrangeMatrix = [[row[i] for row in matrix] for i in range(4)]
 print(rearrangeMatrix)
 
+rearrangeMatrix = matrix
+print(rearrangeMatrix)
+
 rearrangeMatrix = list(zip(*matrix))
 print(rearrangeMatrix)
 
@@ -40,3 +43,4 @@ print(rearrangeMatrix)
 
 lst1 = [2,3,5,1,3]
 print(max(lst1))
+
